0eb1c05288ca016ff1207f158db3e52546cb3f99
Joyeta <joyetahpr@gmail.com>
2022-02-22 13:56:11 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDgQvoHBZz9lFQMd2AteXJESg6BII0DoPU",
+
+    authDomain: "wechat-1431f.firebaseapp.com",
+
+    projectId: "wechat-1431f",
+
+    storageBucket: "wechat-1431f.appspot.com",
+
+    messagingSenderId: "1033736907449",
+
+    appId: "1:1033736907449:web:12d48cb7a4b6554f504968",
+
+    measurementId: "G-R1M5D52JF4"
+
+};
